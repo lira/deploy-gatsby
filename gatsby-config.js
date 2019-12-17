5256635f255b0e4afd9ff4afede6564bcfbabbee
Diego Fernandes <diego.schell.f@gmail.com>
2019-12-17 17:13:49 -0300

    feat: setup initial structure

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,50 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const siteMetadata = require('./config/metadata');
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Rocketseat`,
+        short_name: `Rocketseat`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#7159c1`,
+        display: `standalone`,
+        icon: `src/assets/icon.png` // 512x512
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: `#7159c1`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-98273729",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-offline`,
+  ]
 }
